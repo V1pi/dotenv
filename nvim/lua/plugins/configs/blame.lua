870365753855ed0f765c8c3c2ca19fe9514e52fd
v1pi <vimivini99@gmail.com>
2023-10-19 07:48:30 -0300

    chore: add another gitblame shortcut

--- nvim/lua/plugins/configs/blame.lua
+++ nvim/lua/plugins/configs/blame.lua
@@ -5,3 +5,4 @@ require('gitblame').setup {
 
 local opts = { noremap=true, silent=true }
 vim.keymap.set('n', '<Leader>gb', ':GitBlameToggle<CR>', opts)
+vim.keymap.set('n', '<Leader>gc', ':GitBlameCopyCommitURL<CR>', opts)
